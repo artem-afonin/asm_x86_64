912b57b0c8ed79e9c797571df9b7ddac50c7a132
Artem Afonin <artem@azul.com>
2021-07-27 21:54:14 +0700

    strlength: initial implementation

---
+++ str/strlength/strlength.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "string.h"
+
+extern int __strlength(char *str);
+
+int main(int argc, char **argv) {
+    char str[128] = "Hello, World!";
+    int length = __strlength(str);
+    fprintf(stdout, "str: %s\nlen: %d\n", str, length);
+    return 0;
+}
