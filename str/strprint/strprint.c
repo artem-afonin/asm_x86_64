f347e217a5cb4ba1fb88efe64b10a18d15447451
Artem Afonin <artem@azul.com>
2021-07-27 21:53:58 +0700

    strprint: initial implementation

---
+++ str/strprint/strprint.c
@@ -0,0 +1,10 @@
+#include "string.h"
+
+extern void __strprint(char *str, int length);
+
+int main(int argc, char **argv) {
+    char str[128] = "Hello, World!\n";
+    int length = strlen(str);
+    __strprint(str, length);
+    return 0;
+}
